7cb0ce3dcac537d3107afe30e75e1267533faa40
padmavathyprasannakumar <padmavathyprasannakumar@gmail.com>
2026-06-22 20:47:12 +0800

    Update urls.py

--- payslip_api/payslip_api/urls.py
+++ payslip_api/payslip_api/urls.py
@@ -14,12 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.urls import path, include
+from django.http import JsonResponse
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import include, path
+
+
+def home(request):
+    return JsonResponse({
+        "message": "Payslip API is running successfully"
+    })
+
 
 urlpatterns = [
+    path("", home),
     path("admin/", admin.site.urls),
     path("api/", include("payroll.urls")),
 ]
